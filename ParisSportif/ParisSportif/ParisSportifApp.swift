21a0b9ad996f8d995b225d0d772df9e5435ac4cd
Reda Mimouni <reda.mimouni.ext@adevinta.com>
2023-12-20 13:59:37 +0100

    Init project

---
+++ ParisSportif/ParisSportif/ParisSportifApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParisSportifApp.swift
+//  ParisSportif
+//
+//  Created by reda.mimouni.ext on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParisSportifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
